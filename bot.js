e09cf21c36938822aba7c3c15d9545950e32e7a0
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-09-01 08:45:33 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ const Language = require('./language');
 const Lang = Language.getString('updater');
 
 // Sql
-const WhatsAsenaDB = config.DATABASE.define('WhatsAsenaDuplicated', {
+const amazoneDB = config.DATABASE.define('amazone', {
     info: {
       type: DataTypes.STRING,
       allowNull: false
